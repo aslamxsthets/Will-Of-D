0dba5f6ce41d7ddf5ee08ba84f2fe0a3cdb73b89
AJ <risliya2000@gmail.com>
2026-09-23 02:38:06 +0530

    Update Discord invite URL in Footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import { Instagram, Linkedin, Github } from 'lucide-react';
 import { navItems } from '../../data/navigation';
 
-const discordInviteUrl = 'https://discord.gg/TtaKc364H3';
+const discordInviteUrl = 'https://discord.gg/Wzxd2JS6aM';
 
 function DiscordIcon({ size = 20 }: { size?: number }) {
   return (
